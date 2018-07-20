6c7f98b14c5a59f81e3504100f6d33d95cc9f010
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-07-20 15:09:28 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+//process only when method id post
+if($method == "POST"){
+	@requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+	$text = $json->result->parameters->text;
+	switch($text){
+		case 'hi':
+		$speech = "Hi, Nice to meet you";
+		break;
+		
+		case 'bye':
+		$speech = "Bye, good night";
+		break;
+
+		case 'anything':
+		$speech = "Sorry, I didnot get that."
+		break;
+	
+	}
+
+$response = new \stdClass();
+$response->speech = "";
+$response->displayText = "";
+$response->source = "webhook";
+echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+?>
