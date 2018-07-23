756658234ac4ce0100b41eb49b3b4ee58667f919
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-07-23 13:14:46 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,13 +9,13 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	$text = $json->result->parameters->text;
-	if ($text=='DATABASE')
+	if ($text=='hana')
 	{
 		$speech="SAP HANA is an in-memory, column-oriented, relational database management system";
 	}
 	else
 	{
-		$speech="Input something else";
+		$speech="Input anything else";
 	}
 $response = new \stdClass();
     $response->fulfillmentText = $speech;
