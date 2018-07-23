8fc4de6cd59a95e4370674b0e3598748e8788457
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-07-23 16:12:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -12,61 +12,61 @@ if($method == 'POST'){
 	if ($text=='hana')
 	{
 		$speech="SAP HANA is an in-memory, column-oriented, relational database management system";
-		$driver = 'HDBODBC';
+/*		$driver = 'HDBODBC';
 
-// Host
-//
-$host = "10.70.177.14:30015";
+		// Host
+		//
+		$host = "10.70.177.14:30015";
 
-// Default name of your hana instance
-$db_name = "CH1";
+		// Default name of your hana instance
+		$db_name = "CH1";
 
-// Username
-$username = 'SANYAM_K';
+		// Username
+		$username = 'SANYAM_K';
 
-// Password
-$password = "Welcome@123";
+		// Password
+	$password = "Welcome@123";
 
-// Try to connect
-$conn = odbc_connect("Driver=$driver;ServerNode=$host;Database=$db_name;", $username, $password, SQL_CUR_USE_ODBC);
+	// Try to connect
+	$conn = odbc_connect("Driver=$driver;ServerNode=$host;Database=$db_name;", $username, $password, SQL_CUR_USE_ODBC);
 
-if (!$conn)
-{
-    // Try to get a meaningful error if the connection fails
-    echo "Connection failed.\n";
-    echo "ODBC error code: " . odbc_error() . ". Message: " . odbc_errormsg();
+	if (!$conn)
+	{
+    		// Try to get a meaningful error if the connection fails
+    		echo "Connection failed.\n";
+    		echo "ODBC error code: " . odbc_error() . ". Message: " . odbc_errormsg();
 
    
-}
-else
-{
-    // Do a basic select from DUMMY with is basically a synonym for SYS.DUMMY
-    $sql = 'SELECT * FROM DUMMY';
-    $result = odbc_exec($conn, $sql);
-    if (!$result)
-    {
-        echo "Error while sending SQL statement to the database server.\n";
-        echo "ODBC error code: " . odbc_error() . ". Message: " . odbc_errormsg();
-    }
-    else
-    {
-        while ($row = odbc_fetch_object($result))
-        {
-            // Should output one row containing the string 'X'
-            var_dump($row);
-        }
-    }
-    odbc_close($conn);
+	}	
+	else
+	{
+    		// Do a basic select from DUMMY with is basically a synonym for SYS.DUMMY
+    		$sql = 'SELECT * FROM DUMMY';
+    		$result = odbc_exec($conn, $sql);
+    		if (!$result)
+    		{
+		        echo "Error while sending SQL statement to the database server.\n";
+        		echo "ODBC error code: " . odbc_error() . ". Message: " . odbc_errormsg();
+    		}
+    		else
+    		{
+        		while ($row = odbc_fetch_object($result))
+        		{
+            			// Should output one row containing the string 'X'
+            			var_dump($row);
+        		}
+    		}
+    		odbc_close($conn);*/
 	}
 	else
 	{
 		$speech="Input something else";
 	}
-$response = new \stdClass();
-    $response->fulfillmentText = $speech;
-    $response->source = "webhook";
+	$response = new \stdClass();
+    	$response->fulfillmentText = $speech;
+    	$response->source = "webhook";
 	//$response->displayText= "";
-    echo json_encode($response);
+    	echo json_encode($response);
 
 }
 else
