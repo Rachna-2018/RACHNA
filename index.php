66363a987b195d48238c7439bddb7c461c0d03e3
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-07-23 15:49:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ if($method == 'POST'){
 	
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-	$text = $json->result->parameters->text;
+	$text = $json->queryResult->parameters->text;
 	if ($text=='hana')
 	{
 		$speech="input anythin else";
