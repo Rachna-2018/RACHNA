81328099433fe50715f5e7a2beab29fbe1e333af
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-07-23 12:12:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,23 +9,21 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	$text = $json->result->parameters->text;
-	switch($text){
-		case "hi":
+	if ($text=='hi')
+	{
 		$speech = "Hi, Nice to meet you";
-		break;
-		
-		case "bye":
+	}
+	else if ($text=='bye')
+	{
 		$speech = "Bye, good night";
-		break;
-
-		case "anything":
-		$speech = "Yes, You can type anything here";
-		break;
-			
-		default:
-		$speech = "Sorry, Say something else";
-		break;
-	
+	}
+	elseif($text=='anything')
+	{
+		$speech = "Sorry, I didnot get that.";
+	}
+	else
+	{
+		$speech="something else";
 	}
 $response = new \stdClass();
     $response->fulfillmentText = $speech;
