3a2c052da63125f0737ee7e66df6d5bbbf13bc37
Rachna-2018 <41289843+Rachna-2018@users.noreply.github.com>
2018-07-24 15:05:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if($method == 'POST')
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
 	$text = $json->queryResult->parameters->text;
-	if ($text=='hana' || $text=='HANA' || $text == 'Hana')
+	/*if ($text=='hana' || $text=='HANA' || $text == 'Hana')
 	{
 		$speech="SAP HANA is an in-memory, column-oriented, relational database management system";
 		/*		$driver = 'HDBODBC';
@@ -57,7 +57,7 @@ if($method == 'POST')
             			var_dump($row);
         		}
 		}
-    		odbc_close($conn);*/
+    		odbc_close($conn);
 		
 		
 	}
@@ -65,8 +65,8 @@ if($method == 'POST')
 	{
 		
 		$speech="MySQL is an open-source relational database management system (RDBMS).";
-	}
-	else if (strlen($text) > 1){
+	}*/
+	if (strlen($text) > 1){
 	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, "https://www.google.com/search?q=$show");
 	curl_setopt($ch, CURLOPT_HEADER, 0);
